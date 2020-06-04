f57908e92ce09b5e3f2a6e873582d3539f606bc7
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-03 23:43:07 -0700

    v-47 acting upon the store

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div id="app">Got jokes?{{$store.state }}</div>
+  <div id="app">
+    <h3>Got jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <div>{{$store.state }}</div>
+  </div>
 </template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  methods: mapActions(["initJokes"])
+};
+</script>
---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+import * as types from "./mutation-types";
+
+export const initJokes = ({ commit }) => {
+  fetch("https://official-joke-api.appspot.com/jokes/ten", { method: "GET" })
+    .then(response => response.json())
+    .then(json => commit(types.INIT_JOKES, json));
+};
