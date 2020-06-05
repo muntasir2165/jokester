5fc42e379dbc62bba5a33004a90e1e78711e3cb8
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-04 18:56:06 -0700

    v-49 building a joke component

---
+++ src/components/Joke.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-md-6">
+    <div class="joke-card">
+      <div class="card-block">
+        <p>
+          <u>{{joke.setup}}</u>
+        </p>
+        <p>
+          <em>{{joke.punchline}}</em>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["joke"]
+};
+</script>
